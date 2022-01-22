b5437baae282b1ed30ebd8a1d2b92dbf17fdd230
Taylor Anderson <tganderson0@gmail.com>
2022-01-21 23:50:46 -0700

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './component/header/header';
 import Home from './page/Home';
 import About from './page/About';
 import Projects from './page/Projects';
+import Privacy from './page/Privacy';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
+        <Route path='/privacy' element={<Privacy />} />
         <Route path='/' element={<Home />} />
       </Routes>
     </Router>
